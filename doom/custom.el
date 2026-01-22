49719833bd76f67e3ed9adffbaee29a93c92447c
tmonier <tmonier03@gmail.com>
2026-01-22 23:24:00 +0000

    Add files via upload

---
+++ doom/custom.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("fd22a3aac273624858a4184079b7134fb4e97104d1627cb2b488821be765ff17" default))
+ '(ede-project-directories
+   '("/Users/tmonier/Documents" "/Users/tmonier/Documents/latex/nsf grfp"))
+ '(package-selected-packages '(ac-html pandoc pandoc-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
